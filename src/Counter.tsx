6152fc5f7606d2b1af293eb6b670cf691ab5d63e
haechan <kiki9510@gmail.com>
2022-09-25 22:08:35 +0900

    Updated Counter

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -4,12 +4,32 @@ interface CounterProps {
     startNumber: number;
 }
 
+interface CounterState {
+    number: number;
+}
+
 class Counter extends React.Component<CounterProps> {
+    public state = { number: 0 };
+
+    constructor(props: CounterProps) {
+        super(props);
+        this.state.number = props.startNumber;
+    }
+
+    public handlePlusClick = () => {
+        this.setState({ number: this.state.number + 1 });
+    };
+
+    public handleMinusClick = () => {
+        this.setState({ number: this.state.number - 1 });
+    };
+
     public render() {
         return (
             <div>
-                <h1>{this.props.startNumber}</h1>
-                <button>BUTTON</button>
+                <h1>{this.state.number}</h1>
+                <button onClick={this.handlePlusClick}>Plus</button>
+                <button onClick={this.handleMinusClick}>Minus</button>
             </div>
         );
     }
