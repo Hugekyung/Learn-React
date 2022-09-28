3885edb31e00e2a02c5983405919f46fa9da88e4
haechan <kiki9510@gmail.com>
2022-09-28 23:37:56 +0900

    ExampleProps 컴포넌트 App.tsx에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Counter from "Counter";
+import ExampleProps from "exampleProps";
 import React from "react";
 import ShowName from "ShowName";
 
@@ -6,6 +7,7 @@ class App extends React.Component {
     public render() {
         return (
             <div>
+                <ExampleProps />
                 <ShowName name="yanghaechan" />
                 <Counter startNumber={5} />
             </div>
