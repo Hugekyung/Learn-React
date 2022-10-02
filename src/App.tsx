9962abcc7d1e22b8c1e387c75a879c0cb5c5988e
haechan <kiki9510@gmail.com>
2022-10-03 00:07:11 +0900

    useState로 input 상태 관리

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Counter from "Counter";
 import CounterTwo from "CounterTwo";
 import ExampleProps from "exampleProps";
 import Hello from "hello";
+import InputSample from "inputSample";
 import React from "react";
 import ShowName from "ShowName";
 
@@ -14,6 +15,7 @@ class App extends React.Component {
                 <ShowName name="yanghaechan" />
                 {/* <Counter startNumber={5} /> */}
                 <CounterTwo />
+                <InputSample />
             </div>
         );
     }
