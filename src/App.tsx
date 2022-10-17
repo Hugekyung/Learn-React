f4eb0caf5e81f018ae6c6e8eeda5f30e9145fa49
haechan <kiki9510@gmail.com>
2022-10-18 00:25:42 +0900

    useMemo 예제 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Hello from "hello";
 import InputSample from "inputSample";
 import React from "react";
 import ShowName from "ShowName";
+import exampleUM from "./useUseMemo";
 
 class App extends React.Component {
     public render() {
         return (
             <div>
+                <exampleUM />
                 {/* <Hello name="해찬" />
                 <ExampleProps />
                 <ShowName name="yanghaechan" /> */}
---
+++ src/useUseMemo.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useMemo, useState } from "react";
+
+const exampleUM = () => {
+    const [number, setNumber] = useState(0);
+    const [isKorea, setIsKorea] = useState(true);
+
+    const location = useMemo(() => {
+        return {
+            country: isKorea ? "한국" : "외국",
+        };
+    }, [isKorea]);
+
+    useEffect(() => {
+        console.log("useEffect 호출!");
+    }, [location]);
+
+    return (
+        <div>
+            <h2>하루에 몇끼??</h2>
+            <input
+                type="number"
+                value={number}
+                onChange={(e) => setNumber(Number(e.target.value))}
+            />
+            <hr />
+            <h2>어느 나라 고고??</h2>
+            <p>나라 : {location.country}</p>
+            <button onClick={() => setIsKorea(!isKorea)}>비행기 탑승~</button>
+        </div>
+    );
+};
+
+export default exampleUM;
